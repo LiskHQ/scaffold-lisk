6e0d0e8f004e77fc870b7713a6b3a22c2b1a882f
Muhammad Talha <13951043+TalhaMaliktz@users.noreply.github.com>
2025-05-07 17:00:36 +0500

    Update UI to match Lisk's branding

--- packages/nextjs/components/Logo.tsx
+++ packages/nextjs/components/Logo.tsx
@@ -8,23 +8,30 @@ type Props = React.ComponentProps<"svg"> & {
 /**
  * Lisk logo.
  */
-export const Logo: React.FC<Props> = ({ size = 32, ...props }) => {
+export const Logo: React.FC<Props> = ({}) => {
   return (
-    <svg width={size} height={size} viewBox="0 0 24 24" fill="none" {...props}>
-      <g clipPath="url(#clip0_1692_63849)">
-        <path
-          d="M18 0H6C2.68629 0 0 2.68629 0 6V18C0 21.3137 2.68629 24 6 24H18C21.3137 24 24 21.3137 24 18V6C24 2.68629 21.3137 0 18 0Z"
-          fill="#4170F5"
-        />
-        <path
-          d="M12.007 5L10.7339 7.0914L15.3296 14.8704L11.6013 18.9999C11.6013 18.9999 14.5317 18.9828 14.5143 18.9999C14.497 19.0171 18 15.1104 18 15.1104L12.007 5ZM10.3352 7.86269L6 15.1448L9.41609 18.9999H10.7009L12.5791 16.8407H10.4922L8.65217 14.8193L11.6 9.86734L10.3352 7.86269Z"
-          fill="white"
-        />
-      </g>
+    <svg width="48" height="48" viewBox="0 0 48 48" fill="none" xmlns="http://www.w3.org/2000/svg">
+      <rect width="48" height="48" fill="url(#paint0_linear_3560_2908)" />
+      <path
+        d="M23.5182 7L8 31.7208L16.921 40.9983L30.0984 41L39 31.7208L23.5182 7Z"
+        fill="url(#paint1_linear_3560_2908)"
+      />
       <defs>
-        <clipPath id="clip0_1692_63849">
-          <rect width="24" height="24" rx="12" fill="white" />
-        </clipPath>
+        <linearGradient
+          id="paint0_linear_3560_2908"
+          x1="48"
+          y1="24"
+          x2="1.26338e-06"
+          y2="24"
+          gradientUnits="userSpaceOnUse"
+        >
+          <stop stop-color="#E84D31" />
+          <stop offset="1" stop-color="#E84D31" stop-opacity="0" />
+        </linearGradient>
+        <linearGradient id="paint1_linear_3560_2908" x1="39" y1="32.5" x2="8" y2="31.5" gradientUnits="userSpaceOnUse">
+          <stop stop-color="#E84D31" stop-opacity="0" />
+          <stop offset="1" stop-color="#E84D31" />
+        </linearGradient>
       </defs>
     </svg>
   );
