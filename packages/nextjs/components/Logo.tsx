6846304dadc6af99c09773510e7e2c8e1cc20c84
Muhammad Talha <13951043+TalhaMaliktz@users.noreply.github.com>
2025-06-24 17:07:19 +0500

    Replace rest with props

--- packages/nextjs/components/Logo.tsx
+++ packages/nextjs/components/Logo.tsx
@@ -8,7 +8,7 @@ type Props = React.ComponentProps<"svg"> & {
 /**
  * Lisk logo.
  */
-export const Logo: React.FC<Props> = ({ color = "#FFFFFF", size = 32, ...rest }) => {
+export const Logo: React.FC<Props> = ({ color = "#FFFFFF", size = 32, ...props }) => {
   return (
     <svg
       id="Layer_1"
@@ -17,7 +17,7 @@ export const Logo: React.FC<Props> = ({ color = "#FFFFFF", size = 32, ...rest })
       width={size}
       height={size}
       viewBox="0 0 229.1 259"
-      {...rest}
+      {...props}
     >
       <polygon style={{ fill: color }} points="114.4 0 88.5 42.9 173.7 182.5 102.4 259 162.8 259 229.1 187.9 114.4 0" />
       <polygon
